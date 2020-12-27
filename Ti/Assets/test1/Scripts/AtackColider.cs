f4a40aec30010c110ea58962a4fac62b1f77b1bd
TC702 <sas.taisei.1129@gmail.com>
2020-12-27 09:01:36 +0900

    model case is created. and test data is uploaded.

---
+++ Ti/Assets/test1/Scripts/AtackColider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackColider : MonoBehaviour
+{
+    public GameObject selfColider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selfColider.activeSelf)
+        {
+            //Invoke("SetNotActive", 1);
+        }
+    }
+
+    void SetNotActive()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
